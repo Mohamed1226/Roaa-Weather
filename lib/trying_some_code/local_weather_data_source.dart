c723694cbbc4bc66e02200a19bdaa742ed815c82
altawkeel <mohamedun9@gmail.com>
2021-11-30 05:37:58 +0200

    commenting not important code

--- lib/trying_some_code/local_weather_data_source.dart
+++ lib/trying_some_code/local_weather_data_source.dart
@@ -1,19 +1,19 @@
-import 'dart:convert';
-
-import 'package:roaa_weather/features/weather/data/model/weather_model.dart';
-
-import '../core/shared_pref/shar_pref.dart';
-
-class LocalWeatherDataSource {
-  WeatherModel? _country;
-  @override
-  WeatherModel? getSavedWeather() {
-    var countryWeather = CacheHelper.getData("countryWeather");
-    if (countryWeather != null) {
-      _country = WeatherModel.fromjson(jsonDecode(countryWeather));
-    }
-
-      return _country;
-
-  }
-}
+// import 'dart:convert';
+//
+// import 'package:roaa_weather/features/weather/data/model/weather_model.dart';
+//
+// import '../core/shared_pref/shar_pref.dart';
+//
+// class LocalWeatherDataSource {
+//   WeatherModel? _country;
+//   @override
+//   WeatherModel? getSavedWeather() {
+//     var countryWeather = CacheHelper.getData("countryWeather");
+//     if (countryWeather != null) {
+//       _country = WeatherModel.fromjson(jsonDecode(countryWeather));
+//     }
+//
+//       return _country;
+//
+//   }
+// }
--- lib/trying_some_code/remote_weather_data_source.dart
+++ lib/trying_some_code/remote_weather_data_source.dart
@@ -1,76 +1,76 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
-import 'package:roaa_weather/core/app_config.dart';
-import 'package:roaa_weather/features/weather/data/weather_data_source/weather_api.dart';
-import 'package:roaa_weather/core/shared_pref/shar_pref.dart';
-
-class RemoteWeatherDataSource {
-// late Dio dio;
-//   WeatherWebService(){
-//     BaseOptions baseOptions =BaseOptions(
-//       baseUrl: AppConfig.baseUrl,
-//    receiveDataWhenStatusError: true,
-//       connectTimeout: 20*1000,
-//       receiveTimeout: 20*1000,
-//       queryParameters:{
+// import 'dart:convert';
+// import 'dart:io';
+// import 'package:dio/dio.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:roaa_weather/core/app_config.dart';
+// import 'package:roaa_weather/features/weather/data/weather_data_source/weather_api.dart';
+// import 'package:roaa_weather/core/shared_pref/shar_pref.dart';
 //
-//       } ,
-//     );
+// class RemoteWeatherDataSource {
+// // late Dio dio;
+// //   WeatherWebService(){
+// //     BaseOptions baseOptions =BaseOptions(
+// //       baseUrl: AppConfig.baseUrl,
+// //    receiveDataWhenStatusError: true,
+// //       connectTimeout: 20*1000,
+// //       receiveTimeout: 20*1000,
+// //       queryParameters:{
+// //
+// //       } ,
+// //     );
+// //
+// //     dio =Dio(baseOptions);
+// //   }
 //
-//     dio =Dio(baseOptions);
+// // Future getWeather(String country) async {
+// //   var url = apiUrl(country);
+// //   http.Response _response = await appDio.get(url);
+// //
+// //   var _body = jsonDecode(_response.body);
+// //   return _body;
+// // }
+//   @override
+//   Future getWeatherByUserLocation(lat, log) async {
+//     try {
+//       var url = weatherApiLocation(lat, log);
+//       http.Response _response = await http.get(url);
+//       //print(response.body);
+//       if (_response.statusCode == 200) {
+//         CacheHelper.putData(key: "countryWeather", value: _response.body);
+//       }
+//       var _body = jsonDecode(_response.body);
+//       return _body;
+//     } on HttpException catch (error) {
+//       print(error);
+//       rethrow;
+//     } on FormatException catch (error) {
+//       print(error);
+//       rethrow;
+//     } catch (e) {
+//       rethrow;
+//     }
 //   }
-
-// Future getWeather(String country) async {
-//   var url = apiUrl(country);
-//   http.Response _response = await appDio.get(url);
 //
-//   var _body = jsonDecode(_response.body);
-//   return _body;
+//   @override
+//   Future getWeatherByCountryName(String country) async {
+//     try {
+//       var url = weatherApiCountry(country);
+//       http.Response _response = await http.get(url);
+//       //print(response.body);
+//       if (_response.statusCode == 200) {
+//         CacheHelper.putData(key: "countryWeather", value: _response.body);
+//       }
+//
+//       var _body = jsonDecode(_response.body);
+//       return _body;
+//     } on HttpException catch (error) {
+//       print(error);
+//       rethrow;
+//     } on FormatException catch (error) {
+//       print(error);
+//       rethrow;
+//     } catch (e) {
+//       rethrow;
+//     }
+//   }
 // }
-  @override
-  Future getWeatherByUserLocation(lat, log) async {
-    try {
-      var url = weatherApiLocation(lat, log);
-      http.Response _response = await http.get(url);
-      //print(response.body);
-      if (_response.statusCode == 200) {
-        CacheHelper.putData(key: "countryWeather", value: _response.body);
-      }
-      var _body = jsonDecode(_response.body);
-      return _body;
-    } on HttpException catch (error) {
-      print(error);
-      rethrow;
-    } on FormatException catch (error) {
-      print(error);
-      rethrow;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  @override
-  Future getWeatherByCountryName(String country) async {
-    try {
-      var url = weatherApiCountry(country);
-      http.Response _response = await http.get(url);
-      //print(response.body);
-      if (_response.statusCode == 200) {
-        CacheHelper.putData(key: "countryWeather", value: _response.body);
-      }
-
-      var _body = jsonDecode(_response.body);
-      return _body;
-    } on HttpException catch (error) {
-      print(error);
-      rethrow;
-    } on FormatException catch (error) {
-      print(error);
-      rethrow;
-    } catch (e) {
-      rethrow;
-    }
-  }
-}
